a62b3d16dab2247e51ff3b4c8f0b3c5dfeebf7bf
picpoint <picpoint@yandex.ru>
2025-01-29 16:50:06 +0300

    added code for detected memory

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ import psutil
 # for kernel in cpu_load:
 #     print(f"Загрузка {cpu_load.index(kernel) + 1}-го ядра {kernel} %")
 
+# cpu_frequency = psutil.cpu_freq()
+# print(f"Частота процессора - {cpu_frequency.max / 1000} ГГц")
 
-cpu_frequency = psutil.cpu_freq()
-print(f"Частота процессора - {cpu_frequency.max / 1000} ГГц")
+memory = psutil.virtual_memory()
+total_memory = memory.total / 1000000000
+print(f"Оперативная память - {int(total_memory)}Гб")
+
+free_memory = str(memory.available / 1024 / 1024 / 1024)[:3]
+print(f"Свободная память - {free_memory}Гб")
+
+used_memory = str(memory.used / 1024 / 1024 / 1024)[:3]
+print(f"Использованно - {used_memory}Гб")
+
+# swap = psutil.swap_memory()
+# print(swap)
