242d3f0e7b0e4c8a11e3c885e1d47266bae49278
picpoint <picpoint@yandex.ru>
2025-01-30 08:54:22 +0300

    work with added params for disks

--- main.py
+++ main.py
@@ -5,19 +5,39 @@ import psutil
 #
 # for kernel in cpu_load:
 #     print(f"Загрузка {cpu_load.index(kernel) + 1}-го ядра {kernel} %")
-
+#
 # cpu_frequency = psutil.cpu_freq()
 # print(f"Частота процессора - {cpu_frequency.max / 1000} ГГц")
 
-memory = psutil.virtual_memory()
-total_memory = memory.total / 1000000000
-print(f"Оперативная память - {int(total_memory)}Гб")
 
-free_memory = str(memory.available / 1024 / 1024 / 1024)[:3]
-print(f"Свободная память - {free_memory}Гб")
+# memory = psutil.virtual_memory()
+# total_memory = memory.total / 1000000000
+# print(f"Оперативная память - {int(total_memory)}Гб")
+#
+# free_memory = str(memory.available / 1024 / 1024 / 1024)[:3]
+# print(f"Свободная память - {free_memory}Гб")
+#
+# used_memory = str(memory.used / 1024 / 1024 / 1024)[:3]
+# print(f"Использованно - {used_memory}Гб")
 
-used_memory = str(memory.used / 1024 / 1024 / 1024)[:3]
-print(f"Использованно - {used_memory}Гб")
 
 # swap = psutil.swap_memory()
-# print(swap)
+# swap_memory = str(swap.total / 1024 / 1024 / 1024)[:3]
+# print(f"Общая память подкачки - {swap_memory}Гб")
+#
+# swap_used = str(swap.used / 1024 / 1024 / 1024)[:3]
+# print(f"Используемая память подкачки - {swap_used}Гб")
+#
+# swap_free = str(swap.free / 1024 / 1024 / 1024)[:3]
+# print(f"Свободная память подкачки - {swap_free}Гб")
+
+disk_partitions = psutil.disk_partitions(all=False)
+print("Устройства в системе:")
+
+for dev in disk_partitions:
+    print(dev)
+
+for dev in disk_partitions:
+    print(f"Объём дска {dev[0]}")
+    # disk_usage = psutil.disk_usage(f"{dev[0]}")
+    # print(disk_usage[dev[0]] / 1024 / 1024 / 1024)
