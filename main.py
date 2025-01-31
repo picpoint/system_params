4e22b2d2a60c842890297b1b2e933eff2d3d7a1f
picpoint <picpoint@yandex.ru>
2025-01-31 08:56:39 +0300

    work with disks

--- main.py
+++ main.py
@@ -35,9 +35,14 @@ disk_partitions = psutil.disk_partitions(all=False)
 print("Устройства в системе:")
 
 for dev in disk_partitions:
-    print(dev)
+    print(f"Диск {dev[0]} - {dev[2]}")
+    try:
+        disk_volume = psutil.disk_usage(f"{dev[0]}")
+        print(f"Полный объём диска {dev[0]} - { str(disk_volume[0] / 1024 / 1024 / 1024)[:5]}Гб")
+        print(f"Занято {dev[1]} - {str(disk_volume[1] / 1024 / 1024 / 1024)[:5]}Гб")
+        print(f"Свободно {dev[2]} - {str(disk_volume[2] / 1024 / 1024 / 1024)[:5]}Гб")
+    except BaseException as err:
+        print(f"Не возможно определить объём диска {dev[0]}")
+    print()
 
-for dev in disk_partitions:
-    print(f"Объём дска {dev[0]}")
-    # disk_usage = psutil.disk_usage(f"{dev[0]}")
-    # print(disk_usage[dev[0]] / 1024 / 1024 / 1024)
+
